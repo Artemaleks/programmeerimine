44047ffb54631e0ef89f92bf3eb8b5d5380fa4c2
Artjom <artjom@ubuntu.(none)>
2013-11-06 03:27:33 +0400

    1 rida juurde

---
+++ k0dutoo2.py
@@ -0,0 +1,40 @@
+text = " "
+print " " + text.center(50, "-") + " " 
+
+text = "Paradise"
+print "|" + text.rjust(15, ' ') + "                                   |"
+
+text = ""
+print "|" + text.center(50, " ") + "|"
+
+text = "Artjom Aleksejev"
+print "|" + text.rjust(48, " ") + "  |"
+
+text = ""
+print "|" + text.center(50, " ") + "|"
+
+text = "Voru 21-33"    
+print "|" + text.rjust(10, ' ') + "                                        |"
+
+
+text = "65607, Voru"
+print "|" + text.rjust(10, ' ') + "                                       |"
+
+text = "www.paradise.ee"
+print "|" + text.rjust(10, ' ') + "                                   |"
+
+text = "+37255604746"
+print "|" + text.rjust(10, ' ') + "                                      |"
+
+text = "paradise@paradise.ee"
+print "|" + text.rjust(10, ' ') + "                              |"
+
+text = " "
+print " " + text.center(50, "-") + " "
+
+
+A
+A
+A
+A
+
