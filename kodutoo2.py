4bc6873490cad17017e7a0104f45ee9a1928ca10
opilane <opilane@PC215-10.(none)>
2013-11-06 10:38:24 +0200

    1 rida juurde

---
+++ kodutoo2.py
@@ -0,0 +1,37 @@
+text = " "
+print " " + text.center(50, "-") + " "
+
+text = "Paradise"
+print "|" + text.rjust(15, ' ') + "                               	|"
+
+text = ""
+print "|" + text.center(47, " ") + "|"
+
+text = "Artjom Aleksejev"
+print "|" + text.rjust(45, " ") + "  |"
+
+text = ""
+print "|" + text.center(47, " ") + "|"
+
+text = "Voru 21-33"    
+print "|" + text.rjust(10, ' ') + "                                    	|"
+
+
+text = "65607, Voru"
+print "|" + text.rjust(10, ' ') + "                                   	|"
+
+text = "www.paradise.ee"
+print "|" + text.rjust(10, ' ') + "                               	|"
+
+text = "+37255604746"
+print "|" + text.rjust(10, ' ') + "                                  	|"
+
+text = "paradise@paradise.ee"
+print "|" + text.rjust(10, ' ') + "                          	|"
+
+text = " "
+print " " + text.center(50, "-") + " "
+
+ 
+
+
