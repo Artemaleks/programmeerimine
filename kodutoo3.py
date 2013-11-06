2c3cb67ef997f611a887818e0291d77f2b910713
opilane <opilane@PC215-10.(none)>
2013-11-06 12:01:27 +0200

    1 rida juurde

---
+++ kodutoo3.py
@@ -0,0 +1,45 @@
+text = " "
+print "", text.center(65, "-") + "" 
+text = "UUDISKIRI"
+print "|", text.center(65, " ") + "|" 
+text =  "" 
+
+print "|", text.ljust(65," ") + "|"
+text =  "Lugupeetud Aivar!" 
+
+print "|", text.ljust(65," ") + "|"
+text =  "" 
+
+print "|", text.ljust(65," ") + "|"
+text =  "Tahan teavitada, et eile 1.01.2013 puudusin praktikohal."
+
+print "|", text.ljust(65," ") + "|"
+text = "Selle pohjuseks oli mul Tallinnasse minek arsti juurde,"
+
+print "|", text.ljust(65," ") + "|"
+text =  "mis oleks valtimatu."
+
+print "|", text.ljust(65," ") + "|"
+text =  "Loodan, et ei jaanud tahtsamatest tooylesandedest puudu"
+
+print "|", text.ljust(65," ") + "|"
+text = "ja saan nendega kenasti hakkama. "
+
+print "|", text.ljust(65," ") + "|"
+text =  "" 
+
+print "|", text.ljust(65," ") + "|"
+text =  "" 
+
+text = "Artjom Aleksejev"
+print "|", text.rjust(65, " ") + "|" 
+text =  "" 
+
+text = "VKG Praktikant"
+print "|", text.rjust(65, " ") + "|" 
+text =  ""
+
+print "|", text.ljust(65," ") + "|"
+text = " "
+print "", text.center(65, "-") + "" 
+
